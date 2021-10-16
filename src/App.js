6d3a81ab71a9e69be0b03adec91343594b0bcb68
deepak <deepak21devops@gmail.com>
2021-10-16 14:24:29 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Pages/HomePage/Home';
 
 function App() {
   return (
     <>
     <NavBar/>
+    <Home/>
     </>
   );
 }
---
+++ src/Components/Pages/HomePage/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../Header/Header'
+import './home.css'
+
+export default function Home() {
+    return (
+        <div>
+            <Header/>
+            homepage
+        </div>
+    )
+}
