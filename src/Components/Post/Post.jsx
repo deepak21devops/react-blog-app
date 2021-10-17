f7cbadf49002022433287d048ef5dc414904722c
deepak <deepak21devops@gmail.com>
2021-10-17 06:33:43 +0530

    post Done

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import SinglePost from '../SinglePost/SinglePost'
 import './post.css'
 export default function Post() {
     return (
         <div className="post">
-            
+            <SinglePost/>
+            <SinglePost/>
+            <SinglePost/>
+            <SinglePost/>
         </div>
     )
 }
