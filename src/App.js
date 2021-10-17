f3431de1d568bf9c45b1c3bab58e02989602e5c7
deepak <deepak21devops@gmail.com>
2021-10-17 08:56:00 +0530

    Write completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Home from './Components/Pages/HomePage/Home';
 import Single from './Components/Pages/Single/Single';
+import Write from './Components/Pages/Write/Write';
 
 function App() {
   return (
     <>
     <NavBar/>
-    <Single/>
+    <Write/>
     </>
   );
 }
---
+++ src/Components/Pages/Write/Write.jsx
@@ -0,0 +1,26 @@
+import './write.css'
+export default function Write() {
+    return (
+        <div className="write">
+            <div className="writeContainer">
+                <form className="writeGroup">
+                    <img className="writeImg" src="https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+                    <div className="writeContent">
+                        <label htmlFor="fileInput">
+                        <i class="uploadIcon fas fa-plus"></i>
+                        </label>
+                        <input type="file" id="fileInput" style={{display:"none"}}></input>
+
+                        <input className="inputTitle" type="text" placeholder="Title..." autoFocus={true}></input>
+                    </div>
+
+                    <div className="writeContent">
+                        <textarea className="inputText" placeholder="Tell Your Story"></textarea>
+                        <button className="btnPushlish">Publish</button>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
