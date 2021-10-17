3c2b48d89b67245e4d69a75202216a63dd06797a
deepak <deepak21devops@gmail.com>
2021-10-17 19:04:26 +0530

    Login Page Done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Home from './Components/Pages/HomePage/Home';
+import Login from './Components/Pages/Login/Login';
 import Single from './Components/Pages/Single/Single';
 import Settings from './Components/Pages/UserSettings/Settings';
 import Write from './Components/Pages/Write/Write';
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
     <NavBar/>
-    <Settings/>
+    <Login/>
     </>
   );
 }
---
+++ src/Components/Pages/Login/Login.jsx
@@ -0,0 +1,19 @@
+import './login.css'
+
+export default function Login() {
+    return (
+        <div className="login">
+            <span className="loginTitle">Login</span>
+            <form className="loginForm">
+                <label>Email</label>
+                <input type="text" placeholder="Enter your Email.."></input>
+
+                <label>Password</label>
+                <input type="password" placeholder="Enter you Password"></input>
+
+                <button className="loginBtn">Login</button>
+            </form>
+            <button className="registerBtn">Register</button>
+        </div>
+    )
+}
